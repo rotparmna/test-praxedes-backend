ebcdfdf7b985d5fcc893d6a43d4f81f29be3d9e3
Michael <rotpar@gmail.com>
2024-03-17 23:57:21 -0500

    Archivos excluidos

---
+++ test-praxedes-backend-api/Exceptions/IDataErrorException.cs
@@ -0,0 +1,8 @@
+﻿namespace test_praxedes_backend_api.Exceptions
+{
+	public interface IDataErrorException
+	{
+        List<string> DataError { get; set; }
+    }
+}
+
---
+++ test-praxedes-backend-api/Exceptions/ValidatorException.cs
@@ -0,0 +1,18 @@
+﻿namespace test_praxedes_backend_api.Exceptions
+{
+	public class ValidatorException:Exception, IDataErrorException
+    {
+        public List<string> DataError { get; set; } = new List<string>();
+
+        public ValidatorException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+		}
+
+        public ValidatorException(string message)
+            : base(message)
+        {
+        }
+    }
+}
+
